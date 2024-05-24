a88360c603b7121e031be6fea0610333ed72fb6b
Daniel Vaughn <danielbvaughn@gmail.com>
2024-05-24 08:57:07 -0400

    adds adj tree lab

---
+++ lab/adj/README.md
@@ -0,0 +1,13 @@
+
+this lab explores the use of an adjacency list to represent a DOM tree.
+some features I want:
+
+- create/update/delete
+- traversal: next, previous, parent, child
+- multi-node editing
+
+some advanced features I may try to target:
+
+- command-based editing with diff capture
+- undo/redo (dependent on above)
+- dom and cssom updates as downstream effects
---
+++ lab/adj/main.js
@@ -0,0 +1,30 @@
+
+
+const tree = {
+  hierarchy: {
+    node_root: ['node_a', 'node_b'],
+    node_a: [],
+    node_b: [],
+  },
+  styles: {
+    node_root: {
+      base: {
+        width: '100px',
+        height: '100px',
+      },
+    },
+    node_a: {
+      base: {
+        display: 'flex'
+      },
+    },
+    node_b: {
+      base: {
+        border: '1px solid blue',
+      },
+    },
+  },
+  data: {
+
+  },
+}
