cdb6f0588198c2a1a2797374497b9a1d48de0161
Daniel Vaughn <danielbvaughn@gmail.com>
2024-06-09 23:41:20 -0400

    improves benchmark test

--- lab/react-vs-dom/dom.js
+++ lab/react-vs-dom/dom.js
@@ -4,17 +4,39 @@ const root = document.getElementById('root')
 
 const button = document.getElementById('run')
 
-button.onclick = () => {
+const list = []
+
+button.onclick = function bclick() {
   for (let i = 0; i < 10000; i++) {
     appendToList()
   }
 }
 
 function appendToList() {
+  const node = {
+    id: randomId(),
+    width: `${randomNumber(300, 50)}px`,
+    height: `${randomNumber(300, 50)}px`,
+    backgroundColor: randomColor(),
+  }
+
+  list.push(node)
+
+  const e = new CustomEvent('added_node', { detail: node })
+  window.dispatchEvent(e)
+}
+
+window.addEventListener('added_node', function didAddNode(e) {
+  const node = e.detail
+
+  render(node)
+})
+
+function render(node) {
   const element = document.createElement('div')
-  element.id = randomId()
-  element.style.width = `${randomNumber(300, 50)}px`
-  element.style.height = `${randomNumber(300, 50)}px`
-  element.style.backgroundColor = randomColor()
+  element.id = node.id
+  element.style.width = node.width
+  element.style.height = node.height
+  element.style.backgroundColor = node.backgroundColor
   root.appendChild(element)
 }
